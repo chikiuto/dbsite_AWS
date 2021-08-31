8ea58c66cd822c46503734f04a8fe4568db16a6a
tahukibear@gmail.com <tahukibear@gmail.com>
2021-09-01 07:10:16 +0900

    settings DEBUG=False

--- dbsite/settings.py
+++ dbsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False),)
 env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 

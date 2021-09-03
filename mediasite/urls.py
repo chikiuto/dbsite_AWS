e3bdde773152ac11982bc5049f314270bf922ea4
tahukibear@gmail.com <tahukibear@gmail.com>
2021-09-03 13:44:56 +0900

    add category and tag post

--- mediasite/urls.py
+++ mediasite/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path 
-from mediasite.views import IndexView, PostDetailView, CategoryListView, TagListView
+from mediasite.views import (
+    IndexView, 
+    PostDetailView, 
+    CategoryListView, 
+    TagListView, 
+    CategoryPostView,
+    TagPostView,
+)
 
 app_name = 'mediasite'
 urlpatterns = [
@@ -7,4 +14,6 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('tags/', TagListView.as_view(), name='tag_list'),
+    path('category/<str:category_slug>/', CategoryPostView.as_view(), name='category_post'),
+    path('tag/<str:tag_slug>/', TagPostView.as_view(), name='tag_post'),
 ]

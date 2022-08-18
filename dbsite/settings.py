ac379d3e628b086f21a24f652ae8e55b1b91d588
chikiuto <abetokichiro@gmail.com>
2022-08-18 18:20:26 +0900

    add DEBUG = False

--- dbsite/settings.py
+++ dbsite/settings.py
@@ -30,6 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+DEBUG = False
 
 # environment setting
 env = environ.Env()

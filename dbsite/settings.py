2c35d2ca2fdfb12f239f95bc10f2574360143819
chikiuto <abetokichiro@gmail.com>
2022-08-18 21:15:36 +0900

    delete import local settings

--- dbsite/settings.py
+++ dbsite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

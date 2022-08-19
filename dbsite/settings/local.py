14d66df47d5dda5fbba757c464d4deda3fa1157f
chikiuto <abetokichiro@gmail.com>
2022-08-19 09:32:30 +0900

    Initial commnit

---
+++ dbsite/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ctonodfj(w38@i$fy+m_l0at)1j--9oq19wp%a-#+ht6)jx+6#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
